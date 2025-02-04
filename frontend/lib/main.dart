ae05a9fbadf3d49e68c7847a4c9310891e9b9e92
澤田 <vazaluta@gmail.com>
2025-02-04 23:19:32 +0900

    dotenvクラスを初期化する

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'speech_provider.dart';
 
-void main() {
+import 'providers/speech_provider.dart';
+
+void main() async {
+  await dotenv.load(fileName: ".env");
   runApp(ProviderScope(
     child: SpeechToTextApp(),
   ));
