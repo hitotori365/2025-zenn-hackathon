82a79748b49ca8aa760c28fcb658987c8b16cd81
澤田 <vazaluta@gmail.com>
2025-02-08 17:26:00 +0900

    チャット一覧をViewに切り出す

--- frontend/lib/views/speech_screen.dart
+++ frontend/lib/views/speech_screen.dart
@@ -4,6 +4,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../providers/speech_provider.dart';
 import '../utils/message_controller.dart';
 import '../utils/scrolling_controller.dart';
+import 'chat_list_view.dart';
 import 'completion_screen.dart';
 
 class SpeechToTextScreen extends ConsumerStatefulWidget {
@@ -46,37 +47,8 @@ class _SpeechToTextScreenState extends ConsumerState<SpeechToTextScreen> {
         children: [
           Column(
             children: [
-              Expanded(
-                child: ListView.builder(
-                  controller: scrollingController.scrollController,
-                  padding: const EdgeInsets.all(8.0),
-                  itemCount: speechState.messages.length,
-                  itemBuilder: (context, index) {
-                    final isUserMessage = index % 2 == 0;
-                    return Align(
-                      alignment: isUserMessage
-                          ? Alignment.centerRight
-                          : Alignment.centerLeft,
-                      child: Container(
-                        margin: const EdgeInsets.all(8),
-                        padding: const EdgeInsets.all(12),
-                        decoration: BoxDecoration(
-                          color: isUserMessage
-                              ? Colors.blue[100]
-                              : Colors.green[100],
-                          borderRadius: BorderRadius.circular(12),
-                        ),
-                        child: Text(
-                          speechState.messages[index],
-                          style: TextStyle(
-                            color:
-                                isUserMessage ? Colors.black87 : Colors.black,
-                          ),
-                        ),
-                      ),
-                    );
-                  },
-                ),
+              ChatListView(
+                scrollingController,
               ),
               TextButton.icon(
                 onPressed: speechNotifier.changeMicMode,
